5b3ad73656fce1ebd48e5e6787aee4d0df11212c
Adam Dawidziuk <adamdawidziuk@onet.pl>
2023-09-23 20:32:04 +0200

    Game layout

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.tic_tac_toe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.core.view.get
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    var llMain : LinearLayout? = null
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        llMain = findViewById(R.id.llMain)
+
+        llMain
     }
 }
