a0e8b4907725d7247e0ff35e83bd6b672e36afbc
Adam Dawidziuk <adamdawidziuk@onet.pl>
2023-09-25 18:26:03 +0200

    changed if player winning code

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -3,17 +3,16 @@ package com.example.tic_tac_toe
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Im
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity()
 {
+    private var endGame = false
     private var turn = "o"
     private var iv1 : ImageView? = null
     private var iv2 : ImageView? = null
@@ -33,30 +32,29 @@ class MainActivity : AppCompatActivity()
         val tvClick = findViewById<TextView>(R.id.tvClick)
         val tvTurn = findViewById<TextView>(R.id.tvTurn)
         val ivTurn = findViewById<ImageView>(R.id.ivTurn)
-        var textAnim = AnimationUtils.loadAnimation(this, R.anim.text_anim)
+        val textAnim = AnimationUtils.loadAnimation(this, R.anim.text_anim)
 
         tvClick.startAnimation(textAnim)
         tvTurn.startAnimation(textAnim)
         ivTurn.startAnimation(textAnim)
 
-        iv1 = findViewById<ImageView>(R.id.iv1)
-        iv2 = findViewById<ImageView>(R.id.iv2)
-        iv3 = findViewById<ImageView>(R.id.iv3)
-        iv4 = findViewById<ImageView>(R.id.iv4)
-        iv5 = findViewById<ImageView>(R.id.iv5)
-        iv6 = findViewById<ImageView>(R.id.iv6)
-        iv7 = findViewById<ImageView>(R.id.iv7)
-        iv8 = findViewById<ImageView>(R.id.iv8)
-        iv9 = findViewById<ImageView>(R.id.iv9)
-
-
+        iv1 = findViewById(R.id.iv1)
+        iv2 = findViewById(R.id.iv2)
+        iv3 = findViewById(R.id.iv3)
+        iv4 = findViewById(R.id.iv4)
+        iv5 = findViewById(R.id.iv5)
+        iv6 = findViewById(R.id.iv6)
+        iv7 = findViewById(R.id.iv7)
+        iv8 = findViewById(R.id.iv8)
+        iv9 = findViewById(R.id.iv9)
     }
 
     fun onClickImageView(view: View)
     {
         val iv = view as ImageView
 
-        if(iv.drawable == null)
+
+        if(iv.drawable == null && !endGame)
         {
             val cellAnim = AnimationUtils.loadAnimation(this, R.anim.cell_anim)
             iv.startAnimation(cellAnim)
@@ -75,71 +73,65 @@ class MainActivity : AppCompatActivity()
                 iv.tag = "o"
                 turn = "x"
             }
+            checkIfWin()
         }
-        ifWin()
+        else if(endGame)
+        {
+            //TODO: make custom dialog with option to restart game and winning player
+            Toast.makeText(this, "Game ended", Toast.LENGTH_LONG).show()
+        }
+
     }
 
-    private fun ifWin()
+    private fun checkIfWin()
     {
         if(iv1?.tag == iv2?.tag && iv3?.tag == iv1?.tag && iv1?.tag !=null)
         {
-            val pulse = AnimationUtils.loadAnimation(this, R.anim.pulse)
-
-            Toast.makeText(this, "Winner ${iv1?.tag}", Toast.LENGTH_LONG).show()
-            (iv1?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv2?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv3?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            iv1?.startAnimation(pulse)
-            iv2?.startAnimation(pulse)
-            iv3?.startAnimation(pulse)
+            setWinningAnimation(iv1, iv2, iv3)
         }
         else if(iv4?.tag == iv5?.tag && iv4?.tag == iv6?.tag && iv4?.tag !=null)
         {
-            val pulse = AnimationUtils.loadAnimation(this, R.anim.pulse)
-
-            Toast.makeText(this, "Winner ${iv4?.tag}", Toast.LENGTH_LONG).show()
-            (iv4?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv5?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv6?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            iv4?.startAnimation(pulse)
-            iv5?.startAnimation(pulse)
-            iv6?.startAnimation(pulse)
+            setWinningAnimation(iv4, iv5, iv6)
         }
         else if(iv7?.tag == iv8?.tag && iv7?.tag == iv9?.tag && iv7?.tag !=null)
         {
-            val pulse = AnimationUtils.loadAnimation(this, R.anim.pulse)
-
-            Toast.makeText(this, "Winner ${iv7?.tag}", Toast.LENGTH_LONG).show()
-            (iv7?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv8?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv9?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            iv7?.startAnimation(pulse)
-            iv8?.startAnimation(pulse)
-            iv9?.startAnimation(pulse)
+            setWinningAnimation(iv7, iv8, iv9)
         }
         else if(iv1?.tag == iv5?.tag && iv1?.tag == iv9?.tag && iv1?.tag !=null)
         {
-            val pulse = AnimationUtils.loadAnimation(this, R.anim.pulse)
-
-            Toast.makeText(this, "Winner ${iv1?.tag}", Toast.LENGTH_LONG).show()
-            (iv1?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv5?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv9?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            iv1?.startAnimation(pulse)
-            iv5?.startAnimation(pulse)
-            iv9?.startAnimation(pulse)
+            setWinningAnimation(iv1, iv5, iv9)
         }
         else if(iv3?.tag == iv5?.tag && iv3?.tag == iv7?.tag && iv3?.tag !=null)
         {
-            val pulse = AnimationUtils.loadAnimation(this, R.anim.pulse)
-
-            Toast.makeText(this, "Winner ${iv3?.tag}", Toast.LENGTH_LONG).show()
-            (iv3?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv5?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            (iv7?.parent as FrameLayout).setBackgroundColor(Color.parseColor("#BAD507"))
-            iv3?.startAnimation(pulse)
-            iv5?.startAnimation(pulse)
-            iv7?.startAnimation(pulse)
+            setWinningAnimation(iv3, iv5, iv7)
         }
+        else if(iv3?.tag == iv6?.tag && iv3?.tag == iv9?.tag && iv3?.tag !=null)
+        {
+            setWinningAnimation(iv3, iv6, iv9)
+        }
+        else if(iv2?.tag == iv5?.tag && iv2?.tag == iv8?.tag && iv2?.tag !=null)
+        {
+            setWinningAnimation(iv2, iv5, iv8)
+        }
+        else if(iv1?.tag == iv4?.tag && iv1?.tag == iv7?.tag && iv1?.tag !=null)
+        {
+            setWinningAnimation(iv1, iv4, iv7)
+        }
+        else if(iv1?.tag !=null && iv2?.tag !=null && iv3?.tag !=null && iv4?.tag !=null && iv5?.tag !=null
+            && iv6?.tag !=null && iv7?.tag !=null && iv8?.tag !=null && iv9?.tag !=null) endGame = true
+    }
+    private fun setWinningAnimation(imageV1 : ImageView?, imageV2 : ImageView?, imageV3 : ImageView?)
+    {
+        val pulse = AnimationUtils.loadAnimation(this, R.anim.pulse)
+        val colorGreen = Color.parseColor("#BAD507")
+
+        Toast.makeText(this, "Winner ${imageV1?.tag}", Toast.LENGTH_LONG).show()
+        (imageV1?.parent as FrameLayout).setBackgroundColor(colorGreen)
+        (imageV2?.parent as FrameLayout).setBackgroundColor(colorGreen)
+        (imageV3?.parent as FrameLayout).setBackgroundColor(colorGreen)
+        imageV1.startAnimation(pulse)
+        imageV2.startAnimation(pulse)
+        imageV3.startAnimation(pulse)
+        endGame = true
     }
 }
